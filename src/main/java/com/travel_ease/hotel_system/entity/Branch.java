c09e4658cd8e2c8e5e375b17ab35def5fb906694
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 21:28:39 +0530

    feat: add Branch entity with relationships to Hotel, Address, and Room

---
+++ src/main/java/com/travel_ease/hotel_system/entity/Branch.java
@@ -0,0 +1,42 @@
+package com.travel_ease.hotel_system.entity;
+
+
+import com.travel_ease.hotel_system.enums.BranchType;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name="branch")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Branch {
+    @Id
+    @Column(name = "branch_id", length=80)
+    private String branchId;
+
+    @Column(name = "room_count")
+    private int roomCount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "branch_type", nullable = false)
+    private BranchType branchType;
+
+    @Column(name = "branch_name", nullable = false , unique = true)
+    private String branchName;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hotel_id")
+    private Hotel hotel;
+
+    @OneToOne(mappedBy = "branch")
+    private Address address;
+
+    @OneToMany(mappedBy = "branch")
+    private List<Room> rooms;
+
+
+}
