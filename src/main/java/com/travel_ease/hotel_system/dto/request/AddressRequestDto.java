46de1e6166abc40bf2a68e594419e47cea7ba6d8
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 21:26:17 +0530

    feat: add AddressRequestDto for address create/update requests

---
+++ src/main/java/com/travel_ease/hotel_system/dto/request/AddressRequestDto.java
@@ -0,0 +1,20 @@
+package com.travel_ease.hotel_system.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AddressRequestDto {
+    private String addressLine;
+    private String city;
+    private String country;
+    private BigDecimal latitude;
+    private BigDecimal longitude;
+    private String branchId;
+}
