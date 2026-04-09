d2e20381f79715bd2e62dfcf1aa1f3a58c983e56
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 21:40:58 +0530

    feat: add HotelFacilityController for CRUD operations with role-based access

---
+++ src/main/java/com/travel_ease/hotel_system/api/HotelFacilityController.java
@@ -0,0 +1,83 @@
+package com.travel_ease.hotel_system.api;
+
+import com.travel_ease.hotel_system.dto.request.HotelFacilityRequestDto;
+import com.travel_ease.hotel_system.service.HotelFacilityService;
+import com.travel_ease.hotel_system.util.StandardResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import java.sql.SQLException;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/hotel-management/api/v1/hotel-facilities")
+public class HotelFacilityController {
+    private final HotelFacilityService facilityService;
+
+    @PostMapping("/user/create")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<StandardResponseDto> create(
+            @RequestBody HotelFacilityRequestDto dto) {
+        facilityService.create(dto);
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        201, "Facility Saved!", null
+                ),
+                HttpStatus.CREATED
+        );
+    }
+
+    @PutMapping("/admin/update/{id}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<StandardResponseDto> update(
+            @PathVariable("id") String facilityId,
+            @RequestBody HotelFacilityRequestDto dto) throws SQLException {
+        facilityService.update(dto, facilityId);
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        201, "Facility Updated!", null
+                ),
+                HttpStatus.CREATED
+        );
+    }
+
+    @DeleteMapping("/host/delete/{id}")
+    @PreAuthorize("hasAnyRole('SUPER_ADMIN')")
+    public ResponseEntity<StandardResponseDto> delete(
+            @PathVariable("id") String facilityId) throws SQLException {
+        facilityService.delete(facilityId);
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        204, "Facility deleted!", null
+                ),
+                HttpStatus.NO_CONTENT
+        );
+    }
+
+    @GetMapping("/visitor/find-by-id/{id}")
+    public ResponseEntity<StandardResponseDto> findById(
+            @PathVariable("id") String facilityId) throws SQLException {
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        200, "Facility found!", facilityService.findById(facilityId)
+                ),
+                HttpStatus.OK
+        );
+    }
+
+    @GetMapping("/visitor/find-all")
+    public ResponseEntity<StandardResponseDto> findAll(
+            @RequestParam String roomId,
+            @RequestParam int page,
+            @RequestParam int size
+    ) throws SQLException {
+        return new ResponseEntity<>(
+                new StandardResponseDto(
+                        200, "Facility list!", facilityService.findAll(page, size, roomId)
+                ),
+                HttpStatus.OK
+        );
+    }
+}
