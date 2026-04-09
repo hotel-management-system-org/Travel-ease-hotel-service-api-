d2715a6517e3aff68fad27b6328514f24855ec63
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-09 21:45:56 +0530

    chore: add main Spring Boot application entry point for HotelServiceApi

---
+++ src/main/java/com/travel_ease/hotel_system/HotelServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.travel_ease.hotel_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelServiceApiApplication.class, args);
+	}
+
+}
