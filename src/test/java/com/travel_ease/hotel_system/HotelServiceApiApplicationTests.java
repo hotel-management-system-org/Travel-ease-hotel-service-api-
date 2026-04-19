c9b26a886ec2e4f38e26b5c1434c6a8c179a6596
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-04-19 16:22:30 +0530

    Init commit....👍

---
+++ src/test/java/com/travel_ease/hotel_system/HotelServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel_ease.hotel_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
