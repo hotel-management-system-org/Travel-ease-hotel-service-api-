e31a563c840d08e50a56d43461f81eece4b230a7
Thamindu Weeravardana <thaminduweeravardana@gmail.com>
2026-06-09 22:09:05 +0530

    feat: enable scheduling support in HotelServiceApiApplication
    * add @EnableScheduling annotation to enable scheduled tasks
    * prepare application for background jobs such as inventory updates or cleanup tasks

--- src/main/java/com/travel_ease/hotel_system/HotelServiceApiApplication.java
+++ src/main/java/com/travel_ease/hotel_system/HotelServiceApiApplication.java
@@ -2,8 +2,10 @@ package com.travel_ease.hotel_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HotelServiceApiApplication {
 
 	public static void main(String[] args) {
